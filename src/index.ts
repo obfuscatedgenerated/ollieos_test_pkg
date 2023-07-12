37708fd252c86831c08924a23830dad3d5cb4791
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2023-07-12 16:51:20 +0100

    add suffix

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import type { SyncProgram } from "ollieos/src/types";
 export default {
     name: "hwpkg",
     description: "Says hello to the world!",
-    arg_descriptions: {
-    },
+    usage_suffix: "",
+    arg_descriptions: {},
     main: (data) => {
         // extract from data to make code less verbose
         const { term } = data;
